14a29bb2ad2052749173087a74ef9b768fcf087e
Sameer Mandve <mandvesameer@gmail.com>
2024-06-24 17:36:18 +0530

    Final Commit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const MarkAllAsReadBtn = document.querySelector("#mark-all-read-btn");
+const unread = document.querySelectorAll(".unread");
+const unreadCount = document.querySelector("#unread-count");
 
+unreadCount.innerText = unread.length;
+
+MarkAllAsReadBtn.addEventListener("click", () => {
+  const notificationUnread = document.querySelectorAll("div.unread");
+
+  for (let elem of notificationUnread) {
+    elem.classList.remove("unread");
+  }
+
+  unreadCount.innerText = 0;
+});
